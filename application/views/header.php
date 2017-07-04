c98eb4db4d6c28e26180d36cf0ad16ac2806815d
suresh07 <soori_rozee@yahoo.com>
2017-07-04 12:01:16 +0530

    Collection header moved to header file

--- application/views/header.php
+++ application/views/header.php
@@ -96,3 +96,32 @@ else
     </nav>
     <!-- End Navigation
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+    <div class="row first-row">
+        <!-- Column 1 -->
+           <div class="col-md-12 text-center">
+                <ul class="list-inline sub-nav">
+                    <li><a href="<?=BASE_URL?>listing/albums/<?=NEWSPAPERS?>">NEWS PAPER CLIPPINGS</a></li>
+                    <li><a>·</a></li>
+                    <li><a href="<?=BASE_URL?>listing/albums/<?=BROCHURES?>">Brochures</a></li>
+                    <li><a>·</a></li>
+                    <li><a href="<?=BASE_URL?>listing/photoAlbums/<?=PHOTOS?>">Photographs</a></li>
+                    <li><a>·</a></li>
+                    <li><a>Search</a></li>
+                    <li id="searchForm">
+                        <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
+                            <div class="input-group add-on">
+                                <input type="text" class="form-control" placeholder="Keywords" name="description" id="description">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default" onclick="return validateTextField();" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                                </div>
+                                <div id="tooltip">Search key word is required</div>
+                            </div>
+                        </form>
+
+                    </li>
+                </ul>
+            </div>
+    </div>
+</div>
+
--- application/views/listing/albums.php
+++ application/views/listing/albums.php
@@ -3,44 +3,6 @@
 	unset($data['Archive']);
 	$archiveType = $viewHelper->getArchiveType($data[0]->albumID);
 ?>
-<div class="container">
-    <div class="row first-row">
-        <!-- Column 1 -->
-           <div class="col-md-12 text-center">
-                <ul class="list-inline sub-nav">
-                    <li><a href="<?=BASE_URL?>listing/albums/<?=NEWSPAPERS?>">NEWS PAPER CLIPPINGS</a></li>
-                    <li><a>·</a></li>
-                    <li><a href="<?=BASE_URL?>listing/albums/<?=BROCHURES?>">Brochures</a></li>
-<!--
-                    <li><a>·</a></li>
-                    <li><a href="#">Books</a></li>
--->
-                    <li><a>·</a></li>
-                    <li><a href="<?=BASE_URL?>listing/photoAlbums/<?=PHOTOS?>">Photographs</a></li>
-<!--
-                    <li><a>·</a></li>
-                    <li><a href="#">Multimedia</a></li>
-                    <li><a>·</a></li>
-                    <li><a href="#">Journals</a></li>
-                    <li><a>·</a></li>
-                    <li><a href="#">Miscellaneous</a></li>
--->
-                    <li><a>·</a></li>
-                    <li><a>Search</a></li>
-                    <li id="searchForm">
-                        <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
-                            <div class="input-group add-on">
-                                <input type="text" class="form-control" placeholder="Keywords" name="description" id="description">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                                </div>
-                            </div>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-    </div>
-</div>
 <div id="grid" class="container-fluid"  data-page="1" data-go="1">
     <div id="posts">
 <?php foreach ($data as $row) { ?>
